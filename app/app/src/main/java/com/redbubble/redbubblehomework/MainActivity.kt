57105de525bde62bfd94fcfb98f553096ba67889
Rafi <rsadat@seven.com.au>
2021-09-03 23:21:07 +1000

    gradle dependency and navigation component in main activity

--- app/app/src/main/java/com/redbubble/redbubblehomework/MainActivity.kt
+++ app/app/src/main/java/com/redbubble/redbubblehomework/MainActivity.kt
@@ -2,17 +2,23 @@ package com.redbubble.redbubblehomework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.redbubble.redbubblehomework.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
